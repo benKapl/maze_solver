b910cd5c4990a37673c73ad33a57587e6e74d1a0
benKapl <benji1.kaplan@gmail.com>
2024-10-29 17:40:15 +0100

    made corrections to previous commit using boot's solutions

--- main.py
+++ main.py
@@ -1,33 +1,8 @@
-from tkinter import Tk, BOTH, Canvas
+from graphics import Window
 
-class Window():
-    def __init__(self, width, height) -> None:
-        self.width = width
-        self.height = height
-        # Create root
-        self.__root = Tk()
-        self.__root.title("Maze Solver")
-        # Create canvas
-        self.canvas = Canvas(self.__root, {"bg": "white", "bd": f"{height}"})
-        self.canvas.pack() # geometry manager
-        # 
-        self.is_running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-
-
-    def redraw(self):
-        self.__root.update()
-        self.__root.update_idletasks()
-    
-    def wait_for_close(self):
-        self.is_running = True
-        while self.is_running:
-            self.redraw()
-
-    def close(self):
-        self.is_running = False
-        
-
-if __name__ == "__main__":
+def main():
     win = Window(800, 600)
     win.wait_for_close()
+
+if __name__ == "__main__":
+    main()
