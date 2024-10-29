e782dc20e3a5b4f786c8e1077a8ba4bd91bfd3bd
benKapl <benji1.kaplan@gmail.com>
2024-10-29 18:05:08 +0100

    created Point, Line and method to draw them in window

--- graphics.py
+++ graphics.py
@@ -1,5 +1,8 @@
 from tkinter import Tk, BOTH, Canvas
 
+from lines import Line
+
+
 class Window():
     def __init__(self, width, height) -> None:
         # Create root
@@ -7,7 +10,7 @@ class Window():
         self.__root.title("Maze Solver")
         # Create canvas
         self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
-        self.__canvas.pack(FILL=BOTH, expand=1) # geometry manager
+        self.__canvas.pack(fill=BOTH, expand=1) # geometry manager
         # 
         self.__running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
@@ -23,3 +26,28 @@ class Window():
 
     def close(self):
         self.__running = False
+
+    def draw_line(self, line: Line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+
+# class Point():
+#     def __init__(self, x: int, y: int) -> None:
+#         self.x = x
+#         self.y = y
+
+
+# class Line():
+#     def __init__(self, a: Point, b: Point) -> None:
+#         self.__a = a
+#         self.__b = b
+
+#     def draw(self, canvas: Canvas, fill_color: str):
+#         canvas.create_line(
+#             self.__a.x,  #x1
+#             self.__a.y,  #y1
+#             self.__b.x,  #x2
+#             self.__b.y,  #y2
+#             fill=fill_color,
+#             width=2
+#         )
--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from graphics import Window
+from lines import Point, Line
 
 def main():
     win = Window(800, 600)
+
+    x1 = Point(10, 10)
+    y1 = Point(100, 100)
+
+    x2 = Point(300, 40)
+    y2 = Point(200, 500)
+
+    line1 = Line(x1, y1)
+    line2 = Line(x2, y2)
+
+    win.draw_line(line1, "red")
+    win.draw_line(line2, "blue")
+
     win.wait_for_close()
 
 if __name__ == "__main__":
