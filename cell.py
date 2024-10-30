72873c2817917270964cb050a809919530a81888
benKapl <benji1.kaplan@gmail.com>
2024-10-30 13:55:51 +0100

    created method to draw from a cell's center to another

--- cell.py
+++ cell.py
@@ -11,6 +11,7 @@ class Cell:
         self._x2 = None
         self._y1 = None
         self._y2 = None
+        self.center = None
         self._win = win
 
     def draw(self, x1, y1, x2, y2):
@@ -18,6 +19,8 @@ class Cell:
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
+        self.x_center = x1 + (x2 - x1)/2
+        self.y_center = y1 + (y2 - y1)/2
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
@@ -30,3 +33,10 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        line = Line(Point(self.x_center, self.y_center), Point(to_cell.x_center, to_cell.y_center))
+        line_color = "red"
+        if undo:
+            line_color = "grey"
+        self._win.draw_line(line, line_color)
