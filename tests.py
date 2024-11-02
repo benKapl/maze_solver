72a183dc85306ead5d68d25036194fa0d5966202
benKapl <benji1.kaplan@gmail.com>
2024-11-02 16:01:47 +0100

    tests working

--- tests.py
+++ tests.py
@@ -10,3 +10,12 @@ class TestMaze():
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win)
         assert len(m1._cells) == num_cols
         assert len(m1._cells[0]) == num_rows
+
+    def test_maze_create_cells_large(self):
+        win = Window(800, 600)
+
+        num_cols = 16
+        num_rows = 12
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10, win)
+        assert len(m1._cells) == num_cols
+        assert len(m1._cells[0]) == num_rows
